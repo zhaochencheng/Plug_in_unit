a467666d2f493944aa47125d72398c905f78ed90
zhaochencheng <907779487@qq.com>
2018-06-22 17:36:09 +0800

    mysql

--- Mongo_init/Mongo_Client.py
+++ Mongo_init/Mongo_Client.py
@@ -37,39 +37,42 @@ import datetime
 import json
 from Plug_in_unit.public_script.function_public import *
 
-# class Mongo():
-#     def __init__(self):
-#         self.host = get_data("mongo","mongohost")
-#         self.port = int(get_data("mongo","port"))
-#         self.database = get_data("mongo","database")
-#         self.collection = get_data("mongo","collection")
-#         self.data = get_data("mongo","data")
-#
-#     def connect_mongo(self):
-#         client = MongoClient(self.host,port=self.port)
-#         #获取数据库
-#         db = client[self.database]
-#         print "当前连接数据库为：",db
-#         #获取集合
-#         collections = db[self.collection]
-#         print "当前获取集合（表）:",collections
-#         return collections
-#     def mongo_insert(self):
-#         client = pymongo.MongoClient(self.host,self.port)
-#         db = client[self.database]
-#         collections = db[self.collection]
-#         print collections.count()
-#         collections.insert_one()
-#         print collections.count()
-#         # print self.connect_mongo().count()
-#         # self.connect_mongo().insert_one(self.data)
-#         # print self.connect_mongo().count()
+class Mongo():
+    host = get_data("mongo","mongohost")
+    port = int(get_data("mongo","port"))
+    database = get_data("mongo","database")
+    collection = get_data("mongo","collection")
+    data = get_data("mongo","data")
+    def __init__(self):
+        pass
+    def connect_mongo(self):
+        client = MongoClient(self.host,port=self.port)
+        #获取数据库
+        db = client[self.database]
+        print("当前连接数据库为：",db)
+        #获取集合
+        collections = db[self.collection]
+        print("当前获取集合（表）:",collections)
+        return collections
+    def mongo_insert(self):
+        client = pymongo.MongoClient(self.host,self.port)
+        db = client[self.database]
+        collections = db[self.collection]
+        print(collections.count())
+        collections.insert_one(self.data)
+        print(collections.count())
+        # print self.connect_mongo().count()
+        # self.connect_mongo().insert_one(self.data)
+        # print self.connect_mongo().count()
 
 if __name__ == '__main__':
     data = get_data("mongo","data")
-    print data,type(data)
-    print
-    # print json.loads(text),type(json.loads(text).decode("utf-8").encode("gb2312"))
+    print(data,type(data))
+    print(data.split(","))
+    # print(data1,type(data1))
+
+
+
     # mongo = Mongo()
     # # mongo.connect_mongo()
     # mongo.mongo_insert()
--- Mysql_init/Mysql_Client.py
+++ Mysql_init/Mysql_Client.py
@@ -6,17 +6,67 @@
 # @File    : Mysql_Client.py
 # @Software: PyCharm
 import pymysql
+import sys
 from Plug_in_unit.public_script.function_public import *
-hostip= get_data("mysql","hostip")
-port = get_data("mysql","port")
-username = get_data("mysql","username")
-pwd = get_data("mysql","pwd")
-database = get_data("mysql","database")
+# hostip= get_data("mysql","hostip")
+# port = get_data("mysql","port")
+# username = get_data("mysql","username")
+# pwd = get_data("mysql","pwd")
+# database = get_data("mysql","database")
+#
+# db = pymysql.connect(host= hostip,port=int(port),user=username,password=pwd,database=database )
+# cursor = db.cursor()
+# cursor.execute("select * FROM AdaptMode")
+# # data = cursor.fetchone()
+# data = cursor.fetchall()
+# print(data)
+# db.close()
 
-db = pymysql.connect(host= hostip,port=int(port),user=username,password=pwd,database=database )
-cursor = db.cursor()
-cursor.execute("select * FROM AdaptMode")
-# data = cursor.fetchone()
-data = cursor.fetchall()
-print data
-db.close()
+class MySQL():
+    #初始化数据
+    hostip= get_data("mysql","hostip")
+    port = get_data("mysql","port")
+    username = get_data("mysql","username")
+    pwd = get_data("mysql","pwd")
+    database = get_data("mysql","database")
+    def Mysql_operate(self,operation):
+        '''
+        :param operation: connect mysql and operate it
+        :return:
+        兼容 Python2 and python3
+        '''
+        if sys.version_info.major == 2:
+            #python2 connect mysql
+            try:
+                db = pymysql.connect(host=self.hostip, port=int(self.port), user=self.username, password=self.pwd, database=self.database)
+                cursor = db.cursor()
+                cursor.execute(operation)
+                db.commit()
+                data = cursor.fetchall()
+                cursor.close()
+                db.close()
+                return data
+            except Exception as E:
+                print(E)
+                raise
+        elif sys.version_info.major == 3:
+            #python3 connect mysql
+            try:
+                db = pymysql.connect(host=self.hostip, port=int(self.port), user=self.username, password=self.pwd, database=self.database)
+                cursor = db.cursor()
+                cursor.execute(operation)
+                db.commit()
+                data = cursor.fetchall()
+                print(data)
+                cursor.close()
+                db.close()
+                return data
+            except Exception as E:
+                print(E)
+                raise
+if __name__ == '__main__':
+    mysql = MySQL()
+    mysql.Mysql_operate("SELECT * FROM AdaptInfo WHERE ResID=10;")
+    operation = 'update AdaptInfo set ResVersion= 6 where ResID=7;'
+    # mysql.Mysql_operate(operation)
+    mysql.Mysql_operate("delete from AdaptInfo where ResID = 9 ")
