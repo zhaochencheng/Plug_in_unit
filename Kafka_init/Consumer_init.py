ceae4704432b447e7c4d63a729efdd3f6367800e
zhaochencheng <907779487@qq.com>
2018-06-22 16:25:26 +0800

    add kafka producer and consumer

--- Kafka_init/Consumer_init.py
+++ Kafka_init/Consumer_init.py
@@ -5,7 +5,7 @@
 # @Email   : 907779487@qq.com
 # @File    : Consumer_init.py
 # @Software: PyCharm
-from pykafka import KafkaClient
+# from pykafka import KafkaClient
 import sys
 from Plug_in_unit.public_script.function_public import *
 import logging as log
@@ -20,9 +20,17 @@ class Kafka_consumer():
 
     def Consumer_Init(self):
         '''
-        消费者 向kafka中 pull 数据
+        消费者 向kafka中 pull 消息
+        #函数作用：向kafka中 (pull)拉取 消息
+        #参数说明：hostname 连接Kafka地址：例：localhost:9092
+        #         topic   消息根据Topic进行归类，可以理解为一个队列
+        #         groupID  一个字符串用来指示一组consumer所在的组。相同的groupID表示在一个组里。相同的groupID消费记录offset时，记录的是同一个offset
+        #         zookeeper_hostname =
+        #兼容 Python2 和 python3
         '''
         if sys.version_info.major == 2:
+            #python2 connect kafka
+            from pykafka import KafkaClient
             client = KafkaClient(hosts=self.hostname)
             print(client.topics)
             Topic = client.topics[self.topic]
@@ -35,6 +43,14 @@ class Kafka_consumer():
                 if message is not None:
                     print(message.offset,message.value)
         elif sys.version_info.major == 3:
+            #python3 connect kafka
+            from kafka import KafkaConsumer
+            consumer = KafkaConsumer(self.topic,group_id=self.groupID,bootstrap_servers=[self.hostname])
+            print("consumer start")
+            for message in consumer:
+                print("Consumed Msg -> '%s' on Topic -> '%s' with Offset -> %d" %(message.value.decode('utf-8'), message.topic, message.offset))
+            print("consumer close")
+            consumer.close()
             pass
 if __name__ == '__main__':
     consumer = Kafka_consumer()
--- Kafka_init/Producer_init.py
+++ Kafka_init/Producer_init.py
@@ -5,7 +5,7 @@
 # @Email   : 907779487@qq.com
 # @File    : Producer_init.py
 # @Software: PyCharm
-from pykafka import KafkaClient
+# from pykafka import KafkaClient
 import sys
 from Plug_in_unit.public_script.function_public import *
 import logging as log
@@ -21,19 +21,40 @@ class Kafka_Producer():
     def producer_init(self):
         '''
         生产者 向kafka中 push 消息
+        #函数作用：向kafka中 (push)写入 消息
+        #参数说明：hostname 连接Kafka地址：例：localhost:9092
+        #         topic   消息根据Topic进行归类，可以理解为一个队列
+        #         msg     要发送的消息
+        #兼容 Python2 和 python3
         '''
         if sys.version_info.major == 2:
-            log.basicConfig(level=log.DEBUG)
+            #python2 connect Kafka
+            from pykafka import KafkaClient
+            #链接kafka
             client = KafkaClient(hosts=self.hostname)
+            #答应当前kafka中的topics
             print(client.topics)
             Topic = client.topics[self.topic]
             producer = Topic.get_producer()
             producer.produce(self.msg)
             print(self.msg)
             producer.stop()
+
         elif sys.version_info.major == 3:
-            pass
+            #python3 connect Kafka
+            from kafka import KafkaClient, SimpleProducer
+            kafka = KafkaClient(self.hostname)
+            producer = SimpleProducer(kafka)
+            producer.send_messages(self.topic, bytes("%s" % self.msg, encoding="utf-8"))
+            print("Published msg -> 'topic:%s'  -> msg:%s" % (self.topic, self.msg))
+            # pass
 
 if __name__ == '__main__':
     producer = Kafka_Producer()
     producer.producer_init()
+    # msg = get_data("kafka2","producer_msg")
+    # print(bytes("%s"%msg,encoding="utf-8"))
+    # print(type(msg))
+    # print(type(b'[12,"fdsf"]'))
+    # msg2 = "%s"%msg
+    # print(bytes(msg2,encoding='utf-8'))
